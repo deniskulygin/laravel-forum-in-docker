ecffd520c4b2f93c37e33af1d590b53cc58a18bd
Denis <deniskulygin@gmail.com>
2020-02-24 01:12:02 +0200

    EPISODE 41 - Thread Subscriptions: Part 2

--- forum/routes/web.php
+++ forum/routes/web.php
@@ -32,3 +32,4 @@ Route::patch('/replies/{reply}', 'RepliesController@update');
 Route::post('/replies/{reply}/favorites/', 'FavoritesController@store');
 Route::delete('/replies/{reply}/favorites/', 'FavoritesController@destroy');
 Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
+Route::post('threads/{channel}/{thread}/subscriptions', 'ThreadSubscriptionsController@store')->middleware('auth');
