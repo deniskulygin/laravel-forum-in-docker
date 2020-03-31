333f7dcf74770c9090050c8cb7d5645dff6ac49e
Denis <deniskulygin@gmail.com>
2020-03-31 22:06:41 +0300

    Notification Files

---
+++ forum/app/Http/Controllers/UserNotificationsController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserNotificationsController extends Controller
+{
+    /**
+     * UserNotificationsController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function index()
+    {
+        return auth()->user()->unreadNotifications;
+    }
+
+    /**
+     * @param User $user
+     * @param string $notificationId
+     */
+    public function destroy(User $user, string $notificationId)
+    {
+        $user->notifications()->findOrFail($notificationId)->markAsRead();
+    }
+}
